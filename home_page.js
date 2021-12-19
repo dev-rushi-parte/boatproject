3ac4285629f6c0d62504cd12c2289d0127caa1d1
rushi2784 <96285307+rushi2784@users.noreply.github.com>
2021-12-19 18:41:49 +0530

    Add files via upload

---
+++ home_page.js
@@ -0,0 +1,34 @@
+
+
+
+
+document.querySelector("#gift_card").addEventListener("click",function(){
+    window.location.href="Gift_Cardhtml.html";
+});
+document.querySelector("#account").addEventListener("click",function(){
+    window.location.href="sign_in.html";
+});
+document.querySelector("#bundle_offer").addEventListener("click",function(){
+    window.location.href="bundles_of_joy.html";
+});
+document.querySelector("#daily_deals").addEventListener("click",function(){
+    window.location.href ="daily_deals.html"
+});
+document.querySelector("#cart").addEventListener("click",function(){
+    window.location.href ="cart_page.html"
+});
+document.querySelector("#earn_100").addEventListener("click",function(){
+    window.location.href ="earn_100.html"
+});
+
+
+// document.querySelector("#").addEventListener("click",function(){
+//     window.location.href="giftcard.html";
+// });
+
+// document.querySelector("#addcart_cart").addEventListener("click", function(){
+//   var name = document.querySelector("#product_name1").value;
+//   var price = document.querySelector("#price").value;
+//   console.log(name);
+// });
+
